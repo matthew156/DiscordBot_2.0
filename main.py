685de3c42a0d2fe6125cdc60ee20983a41fe48ca
Matt <matthewastro@gmail.com>
2021-12-21 22:15:03 -0500

    Added Wrapper for Joke API Commit🚀

--- main.py
+++ main.py
@@ -2,43 +2,41 @@ import discord
 import os
 import requests
 import json
+from jokeapi import Jokes  # Import the Jokes class
+
 client = discord.Client()
+
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + "-" + json_data[0]['a']
-    return(quote)
+    quote = json_data[0]['q'] + "\n\n-" + json_data[0]['a']
+    return (quote)
 
-def get_joke():
-    response = requests.get("https://dad-jokes.p.rapidapi.com/random/joke")
-    json_data = json.loads(response.text)
-    joke = json_data['body'][2] + "\n" + json_data['body'][3]
 
-def update_inspiration(message):
-    if "encouragements" in db.key():
-        encouragements = db["encouragements"]
-        encouragements.append(message)
-        db["encouragements"] = encouragements
-    else:
-        db["encouragements"] = [message]
+def setup():
+    j = Jokes()
+    joke = j.get_joke()
+    setup = joke["setup"] +"\n \n" + joke["delivery"]
+    return (setup)
 
-def delete_encouragement(index):
-    encouragements = db["encouragements"]
-    if len(encouragements) >  index:
-        del encouragements
-        db["encouragements"] = encouragements
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format((client)))
+
 
 @client.event
 async def on_message(msg):
     if msg.author == client.user:
         return
-    
+
     if msg.content.startswith("$pickmeup"):
         quote = get_quote()
         await msg.channel.send(quote)
+    if msg.content.startswith("$dadjoke"):
+        joke = setup()
+        await msg.channel.send(joke)
 
 client.run(os.getenv('TOKEN'))
